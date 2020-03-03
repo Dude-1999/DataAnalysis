e38854959d30df7ff499ca2d1a6bf1959e1b013b
DudeOf1999 <47001638+Dude-1999@users.noreply.github.com>
2020-03-04 00:41:06 +0530

    Create Data.R

---
+++ Data.R
@@ -0,0 +1,24 @@
+For installing packages in R,
+```
+install.packages("data.table")
+library("data.table")  
+```
+<!--
+Only used if required package doesn't exist
+-->
+```
+quiz_data <- fread('hw1_data.csv')
+```
+<!---
+sed to read data or importing data in R
+-->
+To Find out total number of rows we use the code given below
+```
+nrows(quiz_data)
+```
+To find out the mssing values in data(Ozone) we use 
+```
+hw1 = read.csv('hw1_data.csv')
+sub = subset(quiz_data, is.na(Ozone))
+nrow(sub)
+```
