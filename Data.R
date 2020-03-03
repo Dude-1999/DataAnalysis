f935817ed9c136c81e2e828741812ca044022352
DudeOf1999 <47001638+Dude-1999@users.noreply.github.com>
2020-03-04 00:43:37 +0530

    Update Data.R

--- Data.R
+++ Data.R
@@ -1,5 +1,5 @@
 For installing packages in R,
-```
+```R
 install.packages("data.table")
 library("data.table")  
 ```
@@ -13,11 +13,11 @@ quiz_data <- fread('hw1_data.csv')
 sed to read data or importing data in R
 -->
 To Find out total number of rows we use the code given below
-```
+```R
 nrows(quiz_data)
 ```
-To find out the mssing values in data(Ozone) we use 
-```
+To find out the missing values in data(Ozone) we use 
+```R
 hw1 = read.csv('hw1_data.csv')
 sub = subset(quiz_data, is.na(Ozone))
 nrow(sub)
