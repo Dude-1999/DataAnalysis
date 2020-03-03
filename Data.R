ec44ac72ee463ac6d49b15b7736b11cd517b41fe
DudeOf1999 <47001638+Dude-1999@users.noreply.github.com>
2020-03-04 00:52:29 +0530

    Update Data.R

--- Data.R
+++ Data.R
@@ -1,14 +1,14 @@
- **For installing packages in R**
-```R
+ For installing packages in R
+
 install.packages("data.table")
 library("data.table")  
-```
 
- **Only used if required package doesn't exist**
+
+ Only used if required package doesn't exist
 quiz_data <- fread('hw1_data.csv')
 <b>sed to read data or importing data in R</b>
 
-<b>To Find out total number of rows we use the code given below</b>
+<To Find out total number of rows we use the code given below<
 
 nrows(quiz_data)
 
